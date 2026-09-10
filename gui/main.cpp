8dbbdc1553c83e51fb810fc4b6c9f2c8848140d3
VHSalvador <victor.hugo.salvador99@gmail.com>
2026-09-10 17:27:08 -0300

    TASK-009: adicionar base Qt Quick

---
+++ gui/main.cpp
@@ -0,0 +1,13 @@
+#include <QGuiApplication>
+#include <QQmlApplicationEngine>
+
+int main(int argc, char *argv[]) {
+    QGuiApplication application(argc, argv);
+    QQmlApplicationEngine engine;
+    const QUrl main_url(QStringLiteral("qrc:/Main.qml"));
+    QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &application, [main_url](QObject *object, const QUrl &url) {
+        if (!object && url == main_url) QCoreApplication::exit(-1);
+    }, Qt::QueuedConnection);
+    engine.load(main_url);
+    return application.exec();
+}
